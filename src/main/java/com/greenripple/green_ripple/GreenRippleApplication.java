32a0c822f04bef6bab29fc2d66e9868c9f9c42a5
giova <giovannitopor@gmail.com>
2025-03-21 19:37:45 +0100

    Aggiunto il progetto iniziale

---
+++ src/main/java/com/greenripple/green_ripple/GreenRippleApplication.java
@@ -0,0 +1,13 @@
+package com.greenripple.green_ripple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenRippleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenRippleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenripple/green_ripple/GreenRippleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenripple.green_ripple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenRippleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
